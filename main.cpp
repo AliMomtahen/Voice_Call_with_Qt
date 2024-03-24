03cedd678b4004a4f5f48b520c12d8a78cdb52d7
Ali <you@example.com>
2024-03-24 17:56:32 +0330

    delete fake gitignore and add sample of read from audio

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
-#include <QCoreApplication>
-#include <iostream>
-#include<string>
-#include<boost/asio.hpp>
-#include<rtc/rtc.h>
 #include <QAudioInput>
-#include<QObject>
-
+#include <QFile>
+#include <QCoreApplication>
+#include<string>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
 
+    QFile outputFile("output.wav");
+    // if (!outputFile.open(QIODevice::WriteOnly QIODevice::Truncate)) {
+    //     qWarning() << "Could not open output file";
+    //     return 1;
+    // }
 
+    QAudioFormat format;
+    format.setSampleRate(44100);
+    format.setChannelCount(2);
+    format.setSampleSize(16);
+    format.setCodec("audio/pcm");
+    format.setByteOrder(QAudioFormat::LittleEndian);
+    format.setSampleType(QAudioFormat::SignedInt);
 
-    return a.exec();
+    QAudioDeviceInfo info = QAudioDeviceInfo::defaultInputDevice();
+    if (!info.isFormatSupported(format)) {
+        qWarning() << "Default format not supported, try to use nearest";
+        format = info.nearestFormat(format);
+    }
+
+    QAudioInput audioInput(info, format);
+    audioInput.start(&outputFile);
+
+    qDebug() << "Recording audio... Press Ctrl+C to stop.";
+    return app.exec();
 }
+
